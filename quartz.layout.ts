1871f9aecf759186dd039c0729e2d35e02dd4391
Joshua Byrd <phocks@gmail.com>
2025-09-03 20:12:26 +1000

    Quartz sync: Sep 3, 2025, 8:12 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -15,8 +15,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/phocks/quartz",
+      Mastodon: "https://bne.social/@phocks",
     },
   }),
 }
@@ -26,7 +26,7 @@ export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.ConditionalRender({
       component: Component.Breadcrumbs(breadcrumbOptions),
-      condition: (page) => page && true //page.fileData.slug !== "index",
+      condition: (page) => page && true, //page.fileData.slug !== "index",
     }),
     Component.ArticleTitle(),
     Component.ContentMeta(),
@@ -56,7 +56,11 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(breadcrumbOptions), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [
+    Component.Breadcrumbs(breadcrumbOptions),
+    Component.ArticleTitle(),
+    Component.ContentMeta(),
+  ],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
