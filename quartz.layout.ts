bd6b14410b424d5d1e1b71d21c4fcd5e90a3f4f8
Joshua Byrd <phocks@gmail.com>
2025-09-08 20:03:04 +1000

    Oh dear what is happening?

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -48,7 +48,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    // Component.Graph(),
+    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
